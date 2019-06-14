1eaa8538a270850cfc3d25b2065b19fa37debf2e
jack <649959033@qq.com>
2019-06-14 14:01:48 +0800

    1

---
+++ DQAndInformationWeb/src/com/ydwlsys/entity/Teacher.java
@@ -0,0 +1,69 @@
+package com.ydwlsys.entity;
+
+public class Teacher {
+
+	private String Name;
+	private int Id;
+	private String Zy;
+	private String Prat;
+	private String Post;
+	private String Score;
+	private String Scre;
+	private String Tr;
+	private String Image;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getZy() {
+		return Zy;
+	}
+	public void setZy(String zy) {
+		Zy = zy;
+	}
+	public String getPrat() {
+		return Prat;
+	}
+	public void setPrat(String prat) {
+		Prat = prat;
+	}
+	public String getPost() {
+		return Post;
+	}
+	public void setPost(String post) {
+		Post = post;
+	}
+	public String getScore() {
+		return Score;
+	}
+	public void setScore(String score) {
+		Score = score;
+	}
+	public String getScre() {
+		return Scre;
+	}
+	public void setScre(String scre) {
+		Scre = scre;
+	}
+	public String getTr() {
+		return Tr;
+	}
+	public void setTr(String tr) {
+		Tr = tr;
+	}
+	public String getImage() {
+		return Image;
+	}
+	public void setImage(String image) {
+		Image = image;
+	}
+	
+}
--- DQAndInformationWeb/src/com/ydwlsys/servlet/TeacherServlet.java
+++ DQAndInformationWeb/src/com/ydwlsys/servlet/TeacherServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ydwlsys.entity.Teacher;
+
 public class TeacherServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -19,5 +23,17 @@ public class TeacherServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		System.out.println("现在已经来到了贾鹏的功能模块！");
+		
+		JSONObject jsonObject = new JSONObject();
+		Teacher teacher=new Teacher();
+		
+		teacher.setId(1);
+		teacher.setName("jiapeng");
+		jsonObject.put("teacher", teacher);
+		
+		resp.getWriter().print(jsonObject);
+		
 	}
 }
